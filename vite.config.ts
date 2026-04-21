ab5cd983f30c6c0696f5d93406adde8a429cc2df
kuro171492-cyber <kuro171492@gmail.com>
2026-04-21 17:56:19 +0300

    qq

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: '.',
-  base: '/FigLi/',
+  base: '/kuro171492-cyber/FigLi/',
   build: {
     outDir: 'dist',
   },
